0b85b5ec686c95d0154d9ce1ddeef3f86be4fc1d
Veenali Newalkar <veenalinewalkar1@gmail.com>
2021-11-12 20:34:39 +0530

    Mobile nav bar done

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-function myFunction() {
-    var ul = document.getElementById("myTopNav");
-    var nav = document.getElementById("nav");
-    // if (x.style.display == "none") {
-    //     x.style.display = "block"
-    // } else {
-    //     x.style.display = "none"
-    // }
-    if (!ul.classList.contains('responsive')) {
-        ul.classList.add("responsive");
-        nav.classList.add('navtp');
-    } else {
-        ul.classList.remove("responsive");
-        nav.classList.remove("navtp");
-    }
+// function myFunction() {
+//     // var ul = document.getElementById("myTopNav");
+//     // var nav = document.getElementById("nav");
+//     // // if (x.style.display == "none") {
+//     // //     x.style.display = "block"
+//     // // } else {
+//     // //     x.style.display = "none"
+//     // // }
+//     // if (!ul.classList.contains('responsive')) {
+//     //     ul.classList.add("responsive");
+//     //     nav.classList.add('navtp');
+//     // } else {
+//     //     ul.classList.remove("responsive");
+//     //     nav.classList.remove("navtp");
+//     // }
 
-}
+
+//     var icon = document.getElementById('icon');
+//     const mobile_nav = document.getElementsByClassName('mobile-nav')[0];
+
+//     icon.addEventListener('click', () => {
+//         if (!mobile_nav.classList.contains('mobile-nav-visibility')) {
+//             mobile_nav.classList.add('mobile-nav-visibility')
+//         } else {
+//             mobile_nav.classList.remove('mobile-nav-visibility')
+//         }
+//     })
+
+// }
+
+var icon = document.getElementById('icon');
+const mobile_nav = document.getElementsByClassName('mobile-nav')[0];
+const mobile_nav_wrapper = document.getElementsByClassName('mobile-nav-wrapper')[0];
+
+icon.addEventListener('click', () => {
+    if (!mobile_nav.classList.contains('mobile-nav-visibility')) {
+        // mobile_nav.style.display = "block"
+        mobile_nav.classList.add('mobile-nav-visibility');
+        mobile_nav_wrapper.classList.add('mobile-nav-wrapper-visibility')
+    }
+    // else {
+    //     // mobile_nav.style.display = "none"
+    //     mobile_nav.classList.remove('mobile-nav-visibility')
+    // }
+})
